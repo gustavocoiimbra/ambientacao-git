be283bda980926a9d21ba5949ef3e701b09bd0ad
Gustavo Coimbra <gustavocoimbra@discente.ufg.br>
2022-11-15 20:18:24 -0300

    #1 - adicionando comentarios

--- algoritmo_principal.py
+++ algoritmo_principal.py
@@ -4,13 +4,15 @@ def ordenacao_selecao(A):
   cont = 0
 
   for i in range(len(A)):
+    # Encontra o elemento mínimo em A
     minimo = i
 
     for j in range(i + 1, len(A)):
       if A[minimo] > A[j]:
         minimo = j
 
-        A[i], A[minimo] = A[minimo], A[i]
+    # Coloca o elemento mínimo na posição correta
+    A[i], A[minimo] = A[minimo], A[i]
 
 A = random.sample(range(-10, 10), 10)
 
