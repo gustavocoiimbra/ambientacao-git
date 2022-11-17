22fe70e380a13fd13afcb2ce7ffb791d1e68ac4b
Gustavo Coimbra <gustavocoimbra@discente.ufg.br>
2022-11-16 22:53:36 -0300

    #3 - adicionado matriz desordenada e ordenada

--- algoritmo_principal.py
+++ algoritmo_principal.py
@@ -1,6 +1,8 @@
+import random
+
 def ordenacao_selecao(A):
       n = len(A)
-
+      
       for i in range(n):
         minimo = i
 
@@ -9,3 +11,11 @@ def ordenacao_selecao(A):
             minimo = j
 
       A[i], A[minimo] = A[minimo], A[i]
+
+A = random.sample(range(-10, 10), 10)
+
+print("Arranjo desordenado:", A)
+
+ordenacao_selecao(A)
+
+print("Arranjo ordenado:", A)
